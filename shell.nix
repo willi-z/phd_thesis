d848de0d6f6859915eab72a0fc217f688e60dc44
Willi Zschiebsch <willi.zschiebsch@gmail.com>
2026-04-08 22:36:44 +0200

    fix: jabref

--- shell.nix
+++ shell.nix
@@ -2,7 +2,12 @@
 
 pkgs.mkShell {
   buildInputs = [
-    # pkgs.jabref
+    pkgs.jdk21
+    pkgs.jabref
+    pkgs.xwayland        # Compatibility layer
+    pkgs.libxkbcommon    # Keyboards on Wayland
+    pkgs.wayland
+    pkgs.libGL
     
     (pkgs.texlive.combine {
       inherit (pkgs.texlive)
@@ -40,4 +45,14 @@ pkgs.mkShell {
         ; 
     })
   ];
+
+shellHook = ''
+    # These three are the "Golden Trio" for Wayland GUI apps
+    export XDG_RUNTIME_DIR=/run/user/$(id -u)
+    export WAYLAND_DISPLAY=$WAYLAND_DISPLAY
+    export DISPLAY=:0 
+    
+    # Force Java to behave on Wayland
+    export _JAVA_AWT_WM_NONREPARENTING=1
+  '';
 }
